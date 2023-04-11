ae7f40e9ccce13eac6548b26403eede1d671e87a
alpersarac <alper.sarac42@gmail.com>
2023-04-12 01:41:06 +0300

    Api Service configuration update

--- studentadminportal-ui/src/app/students/students.component.ts
+++ studentadminportal-ui/src/app/students/students.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StudentService } from './student.service';
 
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
   styleUrls: ['./students.component.css']
 })
-export class StudentsComponent {
+export class StudentsComponent implements OnInit{
+
+  constructor(private studentService:StudentService){ }
+
+  ngOnInit(): void {
+    // Fetch Students
+    this.studentService.getStudents()
+    .subscribe(
+      (successResponse)=>{
+        console.log(successResponse)
+      },
+      (errorResponse)=>{
+        console.log(errorResponse);
+      }
+    );
+  }
 
 }
